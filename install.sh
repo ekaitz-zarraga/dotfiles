4da0e74aa2093a98e4bc7a083ada79ef82e02c22
Ekaitz Zárraga <ekaitz.zarraga@protonmail.com>
2019-01-28 18:33:37 +0100

    ReStow for reinstalling automatically

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Link at home folder
-stow -t ~ bash vim tmux git
+stow -Rt ~ bash vim tmux git
 
 # Link at XDG_CONFIG_HOME if set, if not to $HOME/.config (they should be the same)
-stow -t ${XDG_CONFIG_HOME:=$HOME/.config} nvim
+stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} nvim
