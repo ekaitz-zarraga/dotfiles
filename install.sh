dada7903408d41b16f9e2942e54c85266592c910
Ekaitz Zárraga <ekaitz.zarraga@protonmail.com>
2019-07-26 11:04:12 +0200

    installation for konsole profile

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ stow -Rt ~ bash vim tmux git
 
 # Link at XDG_CONFIG_HOME if set, if not to $HOME/.config (they should be the same)
 stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} nvim
+
+# konsole colors
+stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/konsole" konsole
