0ccff0e72f2e146052c7b93375371ea3b79900ef
Ekaitz Zarraga <ekaitz@elenq.tech>
2019-11-27 08:58:59 +0100

    Remove vim from stow

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Link at home folder
-stow -Rt ~ bash vim tmux git
+stow -Rt ~ bash tmux git
 
 # Link at XDG_CONFIG_HOME if set, if not to $HOME/.config (they should be the same)
 stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} nvim
