21a262169f5f079ee6e93db4101a4e89a48ae610
Ekaitz Zarraga <ekaitz@elenq.tech>
2020-06-30 12:45:20 +0200

    add i3 and i3 status

--- uninstall.sh
+++ uninstall.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Link at home folder
-stow -Rt ~ bash tmux git
+stow -Dt ~ bash tmux git
 
 # Link at XDG_CONFIG_HOME if set, if not to $HOME/.config (they should be the same)
-stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} nvim
+stow -Dt ${XDG_CONFIG_HOME:=$HOME/.config} -D nvim
 
 # konsole colors
 # stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/konsole" konsole
 
 # kitty
-stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/kitty" kitty
+stow -Dt "${XDG_CONFIG_HOME:=$HOME/.config}/kitty" kitty
 
 # Guix channels
-stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/guix" guix
+stow -Dt "${XDG_CONFIG_HOME:=$HOME/.config}/guix" guix
 
 # i3
-stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/i3" i3
-stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/i3status" i3status
+stow -Dt "${XDG_CONFIG_HOME:=$HOME/.config}/i3" i3
+stow -Dt "${XDG_CONFIG_HOME:=$HOME/.config}/i3status" i3status
 # Plasma session environment variables
 #stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/plasma-workspace/env" plasma-env
