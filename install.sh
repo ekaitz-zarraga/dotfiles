7d99aa95fe9873261334d2dc952b30757bd1ba4f
Ekaitz Zarraga <ekaitz@elenq.tech>
2022-04-10 13:03:10 +0200

    Easier install script

--- install.sh
+++ install.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+echo ${XDG_CONFIG_HOME:=$HOME/.config}
+
 # Link at home folder
-stow --dotfiles -Rt ~ bash tmux git
+stow --dotfiles -Rt$HOME bash tmux git
 
 # Link at XDG_CONFIG_HOME if set, if not to $HOME/.config (they should be the same)
-stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} nvim
+stow -Rt $XDG_CONFIG_HOME nvim
 
 # konsole colors
 # stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/konsole" konsole
 
 # kitty
-stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} kitty
+stow -Rt $XDG_CONFIG_HOME kitty
 
 # Guix channels
-stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/guix" guix
+stow -Rt "$XDG_CONFIG_HOME/guix" guix
 
 # i3
-stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} i3
-stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} i3status
+stow -Rt $XDG_CONFIG_HOME i3
+stow -Rt $XDG_CONFIG_HOME i3status
 # Plasma session environment variables
 #stow -Rt "${XDG_CONFIG_HOME:=$HOME/.config}/plasma-workspace/env" plasma-env
 
 # Shepherd user services
-stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} shepherd
+stow -Rt $XDG_CONFIG_HOME shepherd
 
 # Redshift
-stow -Rt ${XDG_CONFIG_HOME:=$HOME/.config} redshift
+stow -Rt $XDG_CONFIG_HOME redshift
