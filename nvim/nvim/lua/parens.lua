74c70a76cc430495b744616bb6195a5ee7e56d43
Ekaitz Zarraga <ekaitz@elenq.tech>
2024-01-27 22:22:49 +0100

    nvim: parens: Add extra useful keybindings

--- nvim/nvim/lua/parens.lua
+++ nvim/nvim/lua/parens.lua
@@ -130,6 +130,49 @@ parpar.setup {
                 repeatable = false,
                 mode = { "o", "v" },
             },
+            ["<localleader>w"] = {
+                function()
+                    -- place cursor and set mode to `insert`
+                    paredit.cursor.place_cursor(
+                    -- wrap element under cursor with `( ` and `)`
+                    paredit.wrap.wrap_element_under_cursor("( ", ")"),
+                    -- cursor placement opts
+                    { placement = "inner_start", mode = "insert" }
+                    )
+                end,
+                "Wrap element insert head",
+            },
+
+            ["<localleader>W"] = {
+                function()
+                    paredit.cursor.place_cursor(
+                    paredit.wrap.wrap_element_under_cursor("(", ")"),
+                    { placement = "inner_end", mode = "insert" }
+                    )
+                end,
+                "Wrap element insert tail",
+            },
+
+            -- same as above but for enclosing form
+            ["<localleader>i"] = {
+                function()
+                    paredit.cursor.place_cursor(
+                    paredit.wrap.wrap_enclosing_form_under_cursor("( ", ")"),
+                    { placement = "inner_start", mode = "insert" }
+                    )
+                end,
+                "Wrap form insert head",
+            },
+
+            ["<localleader>I"] = {
+                function()
+                    paredit.cursor.place_cursor(
+                    paredit.wrap.wrap_enclosing_form_under_cursor("(", ")"),
+                    { placement = "inner_end", mode = "insert" }
+                    )
+                end,
+                "Wrap form insert tail",
+            }
         }
     }
 }
